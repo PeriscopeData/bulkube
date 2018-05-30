83fc21b5240d29fbe4f8c5f606046ae1bd6502cc
Jason Freidman <jason@periscopedata.com>
2018-05-30 09:55:55 -0700

    New tool, bulk_update, for updating kubernetes config files by label selectors
    What: A new tool for manipulating the yaml files backing a kubernetes cluster.
          Takes a directory, image name+version and label selectors.
          Finds all containers with a matching image name and updates them to the new version.
          Also has a -fmt option to machine-format any file.
    
    Why: It'd be great for our kube cluster to always update from git to get versioning and code reviews.
         Updating 10 deployments file-by-file is a pain, this tool automates those updates.

---
+++ cmd/bulkube/main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	v1apps "k8s.io/api/apps/v1"
+	v1core "k8s.io/api/core/v1"
+	"k8s.io/kubernetes/pkg/kubectl/genericclioptions"
+	"k8s.io/kubernetes/pkg/kubectl/genericclioptions/printers"
+	"k8s.io/kubernetes/pkg/kubectl/genericclioptions/resource"
+	"k8s.io/kubernetes/pkg/kubectl/scheme"
+)
+
+var (
+	labelSelectorFlag string
+	imageNameFlag     string
+	imageShaFlag      string
+	pathFlag          string
+	reformatAll       bool
+	t                 interface{}
+)
+
+func init() {
+	flag.StringVar(&labelSelectorFlag, "l", "", "Filter deployments by label.")
+	flag.StringVar(&imageNameFlag, "image", "", "Image to modify. Only modifies containers that match this image/repository. If @sha256: is included, will use that as sha.")
+	flag.StringVar(&imageShaFlag, "sha", "", "Set image version by sha.")
+	flag.StringVar(&pathFlag, "path", ".", "Path to operate in.")
+	flag.BoolVar(&reformatAll, "fmt", false, "Reformat even if version does not change.")
+}
+
+func main() {
+	flag.Parse()
+	if strings.Contains(imageNameFlag, "@sha256:") {
+		splitName := strings.Split(imageNameFlag, "@sha256:")
+		if len(splitName) == 2 {
+			imageNameFlag, imageShaFlag = splitName[0], splitName[1]
+		}
+	}
+
+	if (imageNameFlag == "" || imageShaFlag == "") && !reformatAll {
+		flag.Usage()
+		return
+	}
+
+	r := builder(pathFlag, labelSelectorFlag).Do()
+	if err := r.Err(); err != nil {
+		panic(err)
+	}
+
+	modifiedObjectFiles, err := updateMatchingObjects(r, imageNameFlag, imageShaFlag)
+	if err != nil {
+		panic(err)
+	}
+	writeObjectFiles(modifiedObjectFiles)
+}
+
+func updateMatchingObjects(r *resource.Result, imageName, imageSha string) (map[string][]*resource.Info, error) {
+	var (
+		objectsByFile = map[string][]*resource.Info{}
+		updatedFiles  = map[string]interface{}{}
+	)
+	err := r.Visit(func(info *resource.Info, err error) error {
+		if err != nil {
+			return err
+		}
+		if objectsByFile[info.Source] == nil {
+			objectsByFile[info.Source] = []*resource.Info{}
+		}
+		objectsByFile[info.Source] = append(objectsByFile[info.Source], info)
+		switch o := info.Object.(type) {
+		case *v1apps.Deployment:
+			if updateContainerImage(o.Spec.Template.Spec.Containers, imageName, imageSha) || reformatAll {
+				updatedFiles[info.Source] = t
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	modifiedObjectFiles := map[string][]*resource.Info{}
+	for filename := range updatedFiles {
+		modifiedObjectFiles[filename] = objectsByFile[filename]
+	}
+	return modifiedObjectFiles, nil
+}
+
+func writeObjectFiles(objectMap map[string][]*resource.Info) {
+	for filename, objectList := range objectMap {
+		writeObjectsToFile(objectList, filename)
+	}
+}
+
+func SameObject(a, b *resource.Info) bool {
+	return a.Name == b.Name &&
+		a.Namespace == b.Namespace &&
+		a.Object.GetObjectKind().GroupVersionKind() == b.Object.GetObjectKind().GroupVersionKind()
+}
+
+func replaceUpdatedObjects(allObjects, updatedObjects []*resource.Info) {
+	for idx, obj := range allObjects {
+		if len(updatedObjects) > 0 && SameObject(obj, updatedObjects[0]) {
+			allObjects[idx] = updatedObjects[0]
+			updatedObjects = updatedObjects[1:]
+		}
+	}
+}
+
+func writeObjectsToFile(updatedObjectList []*resource.Info, filename string) (err error) {
+	var (
+		printer    = &printers.YAMLPrinter{}
+		allObjects []*resource.Info
+		configFile io.WriteCloser
+	)
+
+	// Read all objects to include filtered ones
+	if allObjects, err = builder(filename, "").Do().Infos(); err == nil {
+		// Merge new objects into full list
+		replaceUpdatedObjects(allObjects, updatedObjectList)
+		if configFile, err = os.OpenFile(filename, os.O_TRUNC|os.O_WRONLY, 0755); err == nil {
+			defer configFile.Close()
+			for _, info := range allObjects {
+				fmt.Fprintln(configFile, "---")
+				if err = printer.PrintObj(info.Object, configFile); err != nil {
+					return
+				}
+			}
+		}
+	}
+	return
+}
+
+func updateContainerImage(containerList []v1core.Container, imageName, imageSha string) (changed bool) {
+	for idx, c := range containerList {
+		if strings.HasPrefix(c.Image, imageName+":") || strings.HasPrefix(c.Image, imageName+"@sha256") {
+			containerList[idx].Image = fmt.Sprintf("%s@sha256:%s", imageName, imageSha)
+			changed = true
+		}
+	}
+	return
+}
+
+func builder(path, labelSelector string) *resource.Builder {
+	return resource.NewBuilder(genericclioptions.NewConfigFlags()).
+		WithScheme(scheme.Scheme, v1apps.SchemeGroupVersion, v1core.SchemeGroupVersion).
+		LabelSelector(labelSelector).
+		NamespaceParam("default").DefaultNamespace().RequireNamespace().
+		ExportParam(true).
+		Local().
+		Path(true, path).
+		Flatten()
+}
---
+++ cmd/bulkube/test_suite.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"io/ioutil"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestBulkUpdate(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "BulkUpdate Suite")
+}
+
+const gopherTrainerYaml = `---
+apiVersion: v1
+kind: Service
+metadata:
+  name: gopher-trainer
+  labels:
+    k8s-app: gopher-trainer
+spec:
+  ports:
+    - port: 1800
+      targetPort: 1804
+  selector:
+    k8s-app: gopher-trainer
+  type: ClusterIP
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    k8s-app: gopher-trainer
+    k8s-group: arena
+  name: gopher-trainer
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      k8s-app: gopher-trainer
+  template:
+    metadata:
+      labels:
+        k8s-app: gopher-trainer
+    spec:
+      containers:
+        - name: training-sim
+          image: test-repo/training-sim@sha256:ce9762ec9a423afd10ec4cdb929af496cfad1a875298ab86735ae791fe98cca6
+`
+
+const snakeTrainerYaml = `---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  creationTimestamp: null
+  labels:
+    k8s-app: snake-trainer
+  name: snake-trainer
+---
+apiVersion: v1
+data:
+  config.yaml: STUFF
+kind: ConfigMap
+metadata:
+  creationTimestamp: null
+  labels:
+    k8s-app: snake-trainer
+  name: snake-map
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  creationTimestamp: null
+  labels:
+    k8s-app: snake-trainer
+    k8s-group: arena
+  name: snake-trainer
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      k8s-app: snake-trainer
+  strategy: {}
+  template:
+    metadata:
+      creationTimestamp: null
+      labels:
+        k8s-app: snake-trainer
+    spec:
+      containers:
+      - image: test-repo/training-sim@sha256:ce9762ec9a423afd10ec4cdb929af496cfad1a875298ab86735ae791fe98cca6
+        name: training-sim
+        resources: {}
+      - image: test-repo/pit-guard:latest
+        name: pit-operator
+        resources: {}
+status: {}
+`
+
+var _ = Context("Updates matching files", func() {
+	var (
+		tmpDir string
+		err    error
+	)
+
+	BeforeEach(func() {
+		tmpDir, err = ioutil.TempDir("", "bulk_update_test")
+		if err != nil {
+			panic(err)
+		}
+		ioutil.WriteFile(tmpDir+"/gopher_trainer.yaml", []byte(gopherTrainerYaml), 0755)
+		ioutil.WriteFile(tmpDir+"/snake_trainer.yaml", []byte(snakeTrainerYaml), 0755)
+	})
+
+	ExpectFileMatchesContent := func(filename, content string) {
+		data, err := ioutil.ReadFile(filename)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(string(data)).To(Equal(content))
+	}
+
+	It("Updates the version of matching containers", func() {
+		r := builder(tmpDir, "k8s-app=snake-trainer").Do()
+		updatedObjectMap, _ := updateMatchingObjects(r, "test-repo/training-sim", "abcdef")
+		writeObjectFiles(updatedObjectMap)
+		ExpectFileMatchesContent(tmpDir+"/snake_trainer.yaml", strings.Replace(snakeTrainerYaml, "ce9762ec9a423afd10ec4cdb929af496cfad1a875298ab86735ae791fe98cca6", "abcdef", -1))
+		ExpectFileMatchesContent(tmpDir+"/gopher_trainer.yaml", gopherTrainerYaml)
+	})
+
+	It("Writes all objects in the file, even those skipped by the filter", func() {
+		r := builder(tmpDir, "k8s-app=snake-trainer,k8s-group=arena").Do()
+		updatedObjectMap, _ := updateMatchingObjects(r, "test-repo/training-sim", "abcdef")
+		writeObjectFiles(updatedObjectMap)
+		ExpectFileMatchesContent(tmpDir+"/snake_trainer.yaml", strings.Replace(snakeTrainerYaml, "ce9762ec9a423afd10ec4cdb929af496cfad1a875298ab86735ae791fe98cca6", "abcdef", -1))
+		ExpectFileMatchesContent(tmpDir+"/gopher_trainer.yaml", gopherTrainerYaml)
+	})
+
+	DescribeTable("builder()",
+		func(labelSelector string, expectedLength int) {
+			infos, err := builder(tmpDir, labelSelector).Do().Infos()
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(infos).Should(HaveLen(expectedLength))
+		},
+		Entry("snake file", "k8s-app=snake-trainer", 3),
+		Entry("gopher file", "k8s-app=gopher-trainer", 2),
+		Entry("group", "k8s-group=arena", 2),
+		Entry("all", "", 5),
+		Entry("gopher arena", "k8s-group=arena,k8s-app=gopher-trainer", 1),
+	)
+
+	Describe("updateMatchingObject", func() {
+		It("Returns all files that have updated objects", func() {
+			r := builder(tmpDir, "").Do()
+			updatedObjectMap, _ := updateMatchingObjects(r, "test-repo/training-sim", "abcdef")
+			Expect(updatedObjectMap).Should(HaveKey(tmpDir + "/snake_trainer.yaml"))
+			Expect(updatedObjectMap).Should(HaveKey(tmpDir + "/gopher_trainer.yaml"))
+		})
+
+		It("Only returns files that have updated objects", func() {
+			r := builder(tmpDir, "").Do()
+			updatedObjectMap, _ := updateMatchingObjects(r, "test-repo/pit-guard", "abcdef")
+			Expect(updatedObjectMap).Should(HaveKey(tmpDir + "/snake_trainer.yaml"))
+			Expect(updatedObjectMap).ShouldNot(HaveKey(tmpDir + "/gopher_trainer.yaml"))
+		})
+
+		It("Returns all objects in files that have modified updated objects", func() {
+			r := builder(tmpDir, "").Do()
+			updatedObjectMap, _ := updateMatchingObjects(r, "test-repo/training-sim", "abcdef")
+			Expect(updatedObjectMap[tmpDir+"/snake_trainer.yaml"]).Should(HaveLen(3))
+			Expect(updatedObjectMap[tmpDir+"/gopher_trainer.yaml"]).Should(HaveLen(2))
+		})
+	})
+})
